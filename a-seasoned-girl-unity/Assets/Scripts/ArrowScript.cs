cd103d6948863d85a4a647605fa29ffc031a1e99
Jonah Fintz <j.fintz@outlook.com>
2019-01-26 17:44:26 +0100

    Add new Animations, Weapons and Attacs

---
+++ a-seasoned-girl-unity/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// Sent when another object leaves a trigger collider attached to
+    /// this object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponent<Collider2D>().isTrigger = false;
+    }
+}
